b7b32f342f16814773ca65220fdbc2cfcf33c3e6
Amber Prakash Verma <amberprakashverma21@gmail.com>
2023-01-03 00:59:42 +0530

    first code

---
+++ Microsoft/Evaluate Reverse Polish Notation/code1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int evalRPN(vector<string>& tokens) {
+        string t = tokens.back();
+        tokens.pop_back();
+        if(t != "+" && t != "-" && t != "*" && t != "/") return stoi(t);
+        else{
+            long long int b = evalRPN(tokens);
+            long long int a = evalRPN(tokens);
+            if(t == "+") return a + b;
+            else if(t == "-") return a - b;
+            else if(t == "*") return a * b;
+            else return a / b;
+        }
+    }
+int main(){
+
+
+
+    return 0;
+}
